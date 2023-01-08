fb06438031f2deff85927d99e60ee91ecc4c600b
NileshFury <108253062+NileshFury@users.noreply.github.com>
2023-01-08 22:09:50 +0530

    Add files via upload
    week2.day4

---
+++ college/College.java
@@ -0,0 +1,18 @@
+package college;
+
+public class College {
+	
+	public void collegeName() {
+		System.out.println("College is Bharathiar University");
+
+	}
+	public void collegeCode() {
+		System.out.println("College Code is 5555");
+
+	}
+	public void collegeRank() {
+		System.out.println("College Rank is 06");
+
+	}
+
+}
